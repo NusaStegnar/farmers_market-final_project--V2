4fc3d7d52cfa9b00cad8adf6337a3aecf7384078
Nusa Stegnar <sanusa644@gmail.com>
2024-03-13 08:37:27 +0100

    added new database

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sqlalchemy as sa
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///farmers_market.db"
 app.config["SECRET_KEY"] = "my super secret key"
 db.init_app(app)
 
